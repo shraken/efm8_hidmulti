08e12ce3671f830c56fc36fcd21a1ac00b493d09
Nicholas L Shrake <shraken@gmail.com>
2017-06-19 15:50:32 -0700

    initial commit, add sw and fw example with gitignore

---
+++ fw/code/source/F3xx_HIDtoUART.h
@@ -0,0 +1,68 @@
+//-----------------------------------------------------------------------------
+// F3xx_USBtoUART.h
+//-----------------------------------------------------------------------------
+// Copyright 2008 Silicon Laboratories, Inc.
+// http://www.silabs.com
+//
+// Program Description:
+//
+// Stub file for Firmware Template.
+//
+//
+// How To Test:    See Readme.txt
+//
+//
+// Target:         C8051F32x, C8051F34x, C8061T622/3 or 'T326/7
+// Tool chain:     Keil / Raisonance
+//                 Silicon Laboratories IDE version 3.4x
+// Command Line:   See Readme.txt
+// Project Name:   HIDtoUART
+//
+// Release 1.1
+//    -11 SEP 2008 (TP)
+//    -Updated for 'T622/3
+//
+// Release 1.0
+//    -Initial Revision (PD)
+//    -04 JUN 2008
+//
+
+#ifndef  _USBTOUART_H_
+#define  _USBTOUART_H_
+
+/*
+#define IN_CONTROL 0xFE
+#define IN_CONTROL_SIZE 8
+
+#define OUT_CONTROL 0xFD
+#define OUT_CONTROL_SIZE 8
+*/
+
+#define IN_DATA  0x01
+#define IN_DATA_SIZE 63
+#define OUT_DATA 0x02
+#define OUT_DATA_SIZE 60
+
+#define IN_DATA2  0x03
+#define IN_DATA_SIZE2 63
+#define OUT_DATA2 0x04
+#define OUT_DATA_SIZE2 60
+
+void System_Init (void);
+void Usb_Init (void);
+
+extern unsigned char TX_Ready;
+
+extern unsigned char xdata IN_PACKET[];
+extern unsigned char xdata OUT_PACKET[];
+
+extern unsigned char xdata IN_PACKET2[];
+extern unsigned char xdata OUT_PACKET2[];
+
+extern unsigned char USB_OUT_SUSPENDED;
+
+#endif  /* _USB_DESC_H_ */
+
+//-----------------------------------------------------------------------------
+// End of File
+//-----------------------------------------------------------------------------
---
+++ sw/read_test.c
@@ -0,0 +1,152 @@
+/*******************************************************
+ Windows HID simplification
+
+ Alan Ott
+ Signal 11 Software
+
+ 8/22/2009
+
+ Copyright 2009
+ 
+ This contents of this file may be used by anyone
+ for any reason without any conditions and may be
+ used as a starting point for your own applications
+ which use HIDAPI.
+********************************************************/
+
+#include <stdio.h>
+#include <wchar.h>
+#include <string.h>
+#include <stdlib.h>
+#include "hidapi.h"
+
+// Headers needed for sleeping.
+#ifdef _WIN32
+	#include <windows.h>
+#else
+	#include <unistd.h>
+#endif
+
+int main(int argc, char* argv[])
+{
+	int res;
+	unsigned char buf[256];
+	#define MAX_STR 255
+	wchar_t wstr[MAX_STR];
+	hid_device *handle;
+	int i;
+	int count;
+
+#ifdef WIN32
+	UNREFERENCED_PARAMETER(argc);
+	UNREFERENCED_PARAMETER(argv);
+#endif
+
+	struct hid_device_info *devs, *cur_dev;
+
+	if (hid_init())
+		return -1;
+
+	devs = hid_enumerate(0x0, 0x0);
+	cur_dev = devs;	
+	while (cur_dev) {
+		printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls", cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
+		printf("\n");
+		printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
+		printf("  Product:      %ls\n", cur_dev->product_string);
+		printf("  Release:      %hx\n", cur_dev->release_number);
+		printf("  Interface:    %d\n",  cur_dev->interface_number);
+		printf("\n");
+		cur_dev = cur_dev->next;
+	}
+	hid_free_enumeration(devs);
+
+	if (argc < 2) {
+		printf("ERROR: device to open needs to be passed as first argument\r\n");
+		return -1;
+	}
+
+	// open device passed as first argument
+	handle = hid_open_path(argv[1]);
+
+	if (!handle) {
+		printf("unable to open device\n");
+ 		return 1;
+	}
+
+	// Read the Manufacturer String
+	wstr[0] = 0x0000;
+	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
+	if (res < 0)
+		printf("Unable to read manufacturer string\n");
+	printf("Manufacturer String: %ls\n", wstr);
+
+	// Read the Product String
+	wstr[0] = 0x0000;
+	res = hid_get_product_string(handle, wstr, MAX_STR);
+	if (res < 0)
+		printf("Unable to read product string\n");
+	printf("Product String: %ls\n", wstr);
+
+	// Read the Serial Number String
+	wstr[0] = 0x0000;
+	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
+	if (res < 0)
+		printf("Unable to read serial number string\n");
+	printf("Serial Number String: (%d) %ls", wstr[0], wstr);
+	printf("\n");
+
+	// Read Indexed String 1
+	wstr[0] = 0x0000;
+	res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
+	if (res < 0)
+		printf("Unable to read indexed string 1\n");
+	printf("Indexed String 1: %ls\n", wstr);
+
+	// Set the hid_read() function to be non-blocking.
+	hid_set_nonblocking(handle, 1);
+
+	//return 0;
+
+	// Read requested state. hid_read() has been set to be
+	// non-blocking by the call to hid_set_nonblocking() above.
+	// This loop demonstrates the non-blocking nature of hid_read().
+	res = 0;
+	count = 0;
+	while ((res == 0) || (count < 100000)) {
+		res = hid_read(handle, buf, sizeof(buf));
+		//if (res == 0)
+		//	printf("waiting...\n");
+		//if (res < 0)
+		//	printf("Unable to read()\n");
+		if (res > 0) {
+			printf("received new USB packet %d\n", count);
+			printf("Data read:\n   ");
+			// Print out the returned buffer.
+			for (i = 0; i < res; i++)
+				printf("%02hhx ", buf[i]);
+			printf("\n");
+
+			count++;
+		}
+
+		/*
+		#ifdef WIN32
+		Sleep(500);
+		#else
+		usleep(500*1000);
+		#endif
+		*/
+	}
+
+	hid_close(handle);
+
+	/* Free static HIDAPI objects. */
+	hid_exit();
+
+#ifdef WIN32
+	system("pause");
+#endif
+
+	return 0;
+}
